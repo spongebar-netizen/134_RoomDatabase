e6f6d55ae136ba3596402c78c6f8ba2752823fb2
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:09:41 +0700

    menambahkan code dibagian siswaDao

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -1,4 +1,17 @@
 package com.example.pertemuan9.room
 
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+@Dao
 interface SiswaDao {
+    @Query("SELECT * from tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun  insert(siswa: Siswa)
 }
