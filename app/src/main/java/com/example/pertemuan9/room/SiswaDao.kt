8c7e1f0eca2ad81176e60fe0fd3858d813061a01
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:13:27 +0700

    Sedikit mengupdate code dibagian siswaDao karena tdi dibagian flow list siswanya merah

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -1,11 +1,10 @@
 package com.example.pertemuan9.room
 
-import androidx.constraintlayout.helper.widget.Flow
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
@@ -13,5 +12,5 @@ interface SiswaDao {
     fun getAllSiswa(): Flow<List<Siswa>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun  insert(siswa: Siswa)
+    suspend fun insert(siswa: Siswa)
 }
