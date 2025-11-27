e9fc8667174bb67ba2707dd8b7b920e63fab35e0
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:15:49 +0700

    menambahkan code dibagian DataBaseSiswa.kt

--- app/src/main/java/com/example/pertemuan9/room/DataBaseSiswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/DataBaseSiswa.kt
@@ -1,3 +1,26 @@
 package com.example.pertemuan9.room
 
-data class DataBaseSiswa()
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract  fun siswaDao() : SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instace: DatabaseSiswa? = null
+
+
+        fun getDatabase(context: Context): DatabaseSiswa{
+            return (Instace?: synchronized(this){
+                Room.databaseBuilder(
+                    context,DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instace=it }
+            })
+        }
+    }
+}
