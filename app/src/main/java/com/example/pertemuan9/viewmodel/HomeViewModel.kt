e125d4c0e18d104dc6f79ef1e60b6de9db9ca8b0
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:44:04 +0700

    menambahkan val home uistate dan class uistate

--- app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
@@ -1,8 +1,29 @@
 package com.example.pertemuan9.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9.repositori.RepositoriSiswa
+import com.example.pertemuan9.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
 
     companion object{
         private const val TIMEOUT_MILLS = 5_000L
     }
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream()
+        .filterNotNull()
+        .map { HomeUiState(listSiswa = it.toList()) }
+        .stateIn(scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLS),
+            initialValue = HomeUiState())
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+}
