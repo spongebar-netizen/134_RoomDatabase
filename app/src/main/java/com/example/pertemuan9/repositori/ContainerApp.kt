ce0b1e528555e376b2e13ee93ea598e70f827826
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:33:52 +0700

    menambahkan code dibagian ContainerApp.kt

--- app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/pertemuan9/repositori/ContainerApp.kt
@@ -1,2 +1,28 @@
 package com.example.pertemuan9.repositori
 
+
+import android.app.Application
+import android.content.Context
+import com.example.pertemuan9.room.DatabaseSiswa
+
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context) :
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        offlineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+
+    class AplikasiSiswa : Application() {
+        lateinit var container: ContainerApp
+
+        override fun onCreate(){
+            super.onCreate()
+            container = ContainerDataApp(this)
+        }
+    }
+}
