1c082602c19f2d9584bf202f156e155a3bb5e6cb
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 15:56:48 +0700

    menambahkan code dibagian siswa kt

--- app/src/main/java/com/example/pertemuan9/room/Siswa.kt
+++ app/src/main/java/com/example/pertemuan9/room/Siswa.kt
@@ -1,3 +1,15 @@
 package com.example.pertemuan9.room
 
-data class Siswa()
+import android.R
+import androidx.compose.material.icons.Icons
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int =0,
+    val nama: String,
+    val alamat: String,
+    val telepon: String
+)
