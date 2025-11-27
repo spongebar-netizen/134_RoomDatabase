17243fa997d6e6815392c2f5f4e3bb5e4c9508aa
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:54:29 +0700

    menambahkan code dibagian EntryViewModel.kt

--- app/src/main/java/com/example/pertemuan9/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/EntryViewModel.kt
@@ -1,2 +1,16 @@
 package com.example.pertemuan9.viewmodel
 
+import com.example.pertemuan9.room.Siswa
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
+
+fun DetailSiswa.toSiswa(): DetailSiswa
+)
+
+
+fun Siswa.
