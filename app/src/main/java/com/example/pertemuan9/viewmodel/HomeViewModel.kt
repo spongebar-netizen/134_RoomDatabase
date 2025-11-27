040d4c74e9a2c4ad35379df5b6c8a5b6962ae33a
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 16:42:53 +0700

    menambahkan class HomeViewModel.kt

--- app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/HomeViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.pertemuan9.viewmodel
 
-class HomeViewModel {
-}
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+
+    companion object{
+        private const val TIMEOUT_MILLS = 5_000L
+    }
