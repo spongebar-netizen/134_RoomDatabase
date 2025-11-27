3dc88f23626641f15c6038c74a31b522bb96788f
Lalu Fatan <lalufatan25@gmail.com>
2025-11-27 20:32:40 +0700

    menambahkan code dibagian PenyediaViewModel.kt

--- app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.pertemuan9.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan9.repositori.ContainerDataApp
+import com.example.pertemuan9.viewmodel.EntryViewModel
+import com.example.pertemuan9.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+
+Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa*/
+fun CreationExtras.aplikasiSiswa(): ContainerDataApp.AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
+            ContainerDataApp.AplikasiSiswa)
