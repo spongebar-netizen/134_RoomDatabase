076c1210e6001b5ca375f3df76af5e53786903ab
Lalu Fatan <lalufatan25@gmail.com>
2025-12-04 20:16:06 +0700

    memperbaiki beberapa kode PenyediaViewModel.kt

--- app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,13 +1,14 @@
-package com.example.pertemuan9.viewmodel.provider
+package com.example.database.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.pertemuan9.repositori.ContainerDataApp
+import com.example.myroomsiswa.viewmodel.DetailViewModel
+import com.example.pertemuan9.repositori.AplikasiSiswa
 import com.example.pertemuan9.viewmodel.EntryViewModel
 import com.example.pertemuan9.viewmodel.HomeViewModel
-
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
@@ -17,12 +18,22 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+
     }
 }
 
-/**
 
-Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari AplikasiSiswa*/
-fun CreationExtras.aplikasiSiswa(): ContainerDataApp.AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-            ContainerDataApp.AplikasiSiswa)
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
