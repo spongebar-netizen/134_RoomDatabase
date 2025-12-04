069b9d1bb601ba05e7efa1f7034c342c0c49ef68
Lalu Fatan <lalufatan25@gmail.com>
2025-12-04 15:43:58 +0700

    memperbaiki dibagian siswa dao

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -1,16 +1,29 @@
-package com.example.pertemuan9.room
+package com.teoriweek9.teoriweek9.room
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
+import com.example.pertemuan9.room.Siswa
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SiswaDao {
+    // Mengambil semua data siswa, diurutkan berdasarkan nama
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
+    @Query("SELECT * from tblSiswa WHERE id = :id")
+    fun getSiswa(id: Int): Flow<Siswa>
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
+
+    //@Update
+    //suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
