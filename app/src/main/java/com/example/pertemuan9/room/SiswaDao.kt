0d8df0da5cf8836f4cf078f65422fc422a9e472e
Lalu Fatan <lalufatan25@gmail.com>
2025-12-04 20:18:16 +0700

    memperbaiki beberapa kode Siswa

--- app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -21,8 +21,8 @@ interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 
-    //@Update
-    //suspend fun update(siswa: Siswa)
+    @Update
+    suspend fun update(siswa: Siswa)
 
     @Delete
     suspend fun delete(siswa: Siswa)
